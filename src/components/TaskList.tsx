b2e5655e104029a52005cbda77a9dbc963c4cda8
Artur Noura Teixeira <artur.teixeira@serpro.gov.br>
2021-03-11 13:53:47 -0300

    Melhorias

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -21,43 +21,39 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    if (newTaskTitle !== '') {
-      const newTask = {
-        id: Math.floor(Math.random() * 10000),
-        title: newTaskTitle,
-        isComplete: false,
-      };
-
-      setTasks([...tasks, newTask]);
+    if (!newTaskTitle) {
+      return;
     }
+
+    const newTask = {
+      id: Math.floor(Math.random() * 10000),
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const tasksUpdated = tasks.map(task => {
+    const updatedTasks = tasks.map(task => {
       if (task.id === id) {
         task.isComplete = !task.isComplete;
       }
       return task;
     });
 
-    setTasks(tasksUpdated);
+    setTasks(updatedTasks);
   }
 
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
-    const indexToRemove = tasks.findIndex(
-      findTask => findTask.id === id,
-    );
+    const filteredTasks = tasks.filter(task => task.id !== id);
 
-    const result = [
-      ...tasks.slice(0, indexToRemove),
-      ...tasks.slice(indexToRemove + 1),
-    ];
-
-    setTasks(result);
+    setTasks(filteredTasks);
   }
 
 
